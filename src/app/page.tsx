a2e9750058362514c62871d8526ba9c0355f1918
Kinshuk Ghildial <dev.kghildial@gmail.com>
2025-06-16 04:45:08 +0530

    feat(button): add Button component with color coded default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
 import LayoutWrapper from '../components/LayoutWrapper';
 
 const Home = () => {
   return (
     <LayoutWrapper>
       <div className="bg-accent text-accent-foreground">Home</div>
+      <Button variant="default">Click Me</Button>
     </LayoutWrapper>
   );
 };
