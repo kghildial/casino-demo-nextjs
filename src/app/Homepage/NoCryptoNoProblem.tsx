998198d81379257116ac18080775b9112aa94b92
Kinshuk Ghildial <dev.kghildial@gmail.com>
2025-06-16 16:42:13 +0530

    refactor(ui): responsive tweaks to NoCryptoNoProblem section

--- src/app/Homepage/NoCryptoNoProblem.tsx
+++ src/app/Homepage/NoCryptoNoProblem.tsx
@@ -5,11 +5,11 @@ import { Button } from '@/components/ui/button';
 
 const NoCryptoNoProblem = () => {
   return (
-    <div className="mt-5 mb-5 flex items-center rounded-sm bg-primary px-6 py-8">
+    <div className="mt-5 mb-5 flex flex-col items-center rounded-sm bg-primary px-4 py-8 text-center md:flex-row md:px-6 md:text-start">
       <h2 className="text-xl font-bold">
         You don&nbsp;t have Crypto? <br /> No problem.
       </h2>
-      <div className="mx-10 flex gap-4">
+      <div className="mt-2.5 mb-4 flex flex-wrap justify-center gap-0 md:mx-10 md:mt-0 md:mb-0 md:flex-nowrap md:gap-4">
         {paymentIcons.map((icon, index) => (
           <Image
             key={`payment_mode_${index + 1}`}
