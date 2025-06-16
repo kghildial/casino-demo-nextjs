e2289f712f6785a9b70ca67f54d93da9c7e7437b
Kinshuk Ghildial <dev.kghildial@gmail.com>
2025-06-16 20:24:21 +0530

    add select menus in filters

--- src/app/Homepage/SearchNShowcase/Filters.tsx
+++ src/app/Homepage/SearchNShowcase/Filters.tsx
@@ -1,6 +1,18 @@
+import Image from 'next/image';
 import { type FC as ReactFC } from 'react';
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
 import Tag from '@/components/Tag';
+
+import ColletionIcon from '@/assets/icons/menu/collection.svg';
+import ProvisionIcon from '@/assets/icons/menu/providers.svg';
 
 import { IFilters } from './Search.types';
 
@@ -18,6 +30,29 @@ const Filters: ReactFC<IFilters> = ({ tagsList, activeTag, setActiveTag }) => {
           }}
         />
       ))}
+      <Select>
+        <SelectTrigger className="h-[46px] w-[180px] border border-tag-border bg-primary px-1 py-2.5">
+          <Image src={ColletionIcon} width={24} height={24} alt="collections" />
+          <SelectValue placeholder="Collections" />
+        </SelectTrigger>
+        <SelectContent className="border border-tag-border bg-primary">
+          <SelectItem value="1">Collection 1</SelectItem>
+          <SelectItem value="2">Collection 2</SelectItem>
+          <SelectItem value="3">Collection 3</SelectItem>
+        </SelectContent>
+      </Select>
+
+      <Select>
+        <SelectTrigger className="h-[46px] w-[180px] border border-tag-border bg-primary px-1 py-2.5">
+          <Image src={ProvisionIcon} width={24} height={24} alt="provisions" />
+          <SelectValue placeholder="Provisions" />
+        </SelectTrigger>
+        <SelectContent className="border border-tag-border bg-primary">
+          <SelectItem value="1">Provision 1</SelectItem>
+          <SelectItem value="2">Provision 2</SelectItem>
+          <SelectItem value="3">Provision 3</SelectItem>
+        </SelectContent>
+      </Select>
     </div>
   );
 };
